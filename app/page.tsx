2f2aab85e812fba2723279c29226b3ea34573b2d
HassanAlabdulal <hhhalabdulaal@gmail.com>
2024-02-06 01:01:33 +0300

    feat: add a main heading in the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,32 @@
+import { Button } from "@/components/ui/button";
+// import { Player } from "@lottiefiles/react-lottie-player";
+
 export default function Home() {
-  return <main className=" h-screen w-full background"></main>;
+  return (
+    <main className="min-h-screen w-full background">
+      <div className=" flex flex-col items-center justify-center pt-64">
+        <div>
+          <h1 className="text-black lg:text-8xl font-extrabold lg:leading-tight tracking-tight text-5xl text-center ">
+            الأمان المالي بدءًا <br />
+            <span className="text-primary ">من حصادي</span>
+          </h1>
+        </div>
+        <div className="mt-12">
+          <Button className="inline-flex items-center justify-center px-7 py-6 text-xl  shadow-2xl bg-black text-white hover:bg-neutral-800 rounded-3xl">
+            متحمس تعرف أكثر؟ 🔥
+          </Button>
+        </div>
+        <div>
+          {/* <Player
+            src="./animation.json"
+            background="transparent"
+            speed="1"
+            style={{ width: "200px", height: "200px" }}
+            loop
+            autoplay
+          /> */}
+        </div>
+      </div>
+    </main>
+  );
 }
