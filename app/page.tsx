726118a19f98316e13efb67ceab91fdec0958395
HassanAlabdulal <hhhalabdulaal@gmail.com>
2024-02-07 13:30:38 +0300

    enhance: adjust sizes for small screens

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { Player } from "@lottiefiles/react-lottie-player";
 export default function Home() {
   return (
     <main className="min-h-screen w-full background">
-      <div className=" flex flex-col items-center justify-center xl:pt-48 md-pt-44 pt-36">
+      <div className=" flex flex-col items-center justify-center pt-48 max-sm:pt-40">
         <div>
           <h1 className="text-black md:text-8xl font-extrabold md:leading-tight leading-normal tracking-tight text-4xl text-center ">
             الأمان المالي بدءًا <br />
@@ -14,7 +14,7 @@ export default function Home() {
           </h1>
         </div>
         <div className="mt-12">
-          <Button className="inline-flex items-center justify-center px-7 py-7 text-xl  shadow-2xl bg-black text-white hover:bg-neutral-800 rounded-full">
+          <Button className="inline-flex items-center justify-center md:px-7 md:py-7 md:text-xl px-5 py-5 text-lg  shadow-2xl bg-black text-white hover:bg-neutral-800 rounded-full">
             متحمس تعرف أكثر؟ 🔥
           </Button>
         </div>
