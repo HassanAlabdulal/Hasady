91cf8e3ef7c11c51665dc0f139c3d4f163991bea
HassanAlabdulal <hhhalabdulaal@gmail.com>
2024-02-14 12:59:54 +0300

    fix: resolve text size issue in small screens

--- components/learnmore.tsx
+++ components/learnmore.tsx
@@ -6,11 +6,11 @@ export default function Learnmore() {
       id="learnmore"
       className="flex items-start justify-center min-h-screen bg-[#f9f9f9]"
     >
-      <section className="pt-44">
+      <section className="md:pt-44 pt-36">
         <div>
           <h1
             className="text-black md:text-6xl font-extrabold md:leading-tight 
-      leading-normal tracking-tight text-3 text-center"
+      leading-normal tracking-tight text-3xl text-center"
           >
             ليش أستخدم حصادي؟
           </h1>
