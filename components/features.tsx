2b4f2dcf88bd39ca90795a6d9b048a6e0d16c8a9
HassanAlabdulal <hhhalabdulaal@gmail.com>
2024-02-17 03:43:19 +0300

    enhance: adjust margins for small screens

--- components/features.tsx
+++ components/features.tsx
@@ -28,20 +28,20 @@ const serviceList: ServiceProps[] = [
 
 export default function Features() {
   return (
-    <main className="h-screen flex justify-center items-center bg-[#f9f9f9]">
+    <main className="flex justify-center items-center bg-[#f9f9f9]">
       <section className="container py-24 sm:py-32 ">
         <div className="grid lg:grid-cols-[1fr,1fr] gap-8 place-items-center">
           <div>
             <h2
-              className="text-3xl md:text-5xl flex items-center  font-extrabold
-              md:leading-tight tracking-tight"
+              className="text-3xl md:text-5xl flex items-center max-sm:justify-center font-extrabold
+              md:leading-tight tracking-tight "
             >
-              <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
+              <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text  ">
                 مميزاتنا
               </span>
             </h2>
 
-            <p className="text-muted-foreground text-xl mt-4 mb-8 ">
+            <p className="text-muted-foreground text-xl mt-4 mb-8 max-sm:text-center">
               حصادي هو الحل الأمثل لإدارة استثماراتك بذكاء وتحقيق أهدافك
               المالية.
             </p>
@@ -49,13 +49,13 @@ export default function Features() {
             <div className="flex flex-col gap-8">
               {serviceList.map(({ icon, title, description }: ServiceProps) => (
                 <Card key={title} className="bg-[#f7f7f7] shadow-md">
-                  <CardHeader className="space-y-1 flex md:flex-row justify-start items-start gap-4">
-                    <div className="mt-1 bg-primary/20 p-1 rounded-2xl">
-                      {icon}
-                    </div>
+                  <CardHeader className="flex md:flex-row justify-start items-start gap-4 max-sm:gap-0">
+                    <div className=" p-1 rounded-2xl ">{icon}</div>
                     <div>
-                      <CardTitle>{title}</CardTitle>
-                      <CardDescription className="text-md mt-2">
+                      <CardTitle className="max-sm:text-center">
+                        {title}
+                      </CardTitle>
+                      <CardDescription className="text-md mt-2 max-sm:text-center">
                         {description}
                       </CardDescription>
                     </div>
@@ -68,7 +68,7 @@ export default function Features() {
           <img
             src="/assets/featuers.svg"
             className="w-[300px] md:w-[500px] lg:w-[600px] object-contain"
-            alt="About services"
+            alt="services"
           />
         </div>
       </section>
