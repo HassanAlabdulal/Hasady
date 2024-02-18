4519e51797bcf36cb5f0377c557647f3d0653589
HassanAlabdulal <hhhalabdulaal@gmail.com>
2024-02-18 04:58:43 +0300

    feat: add scrolling animations to learnmore page

--- components/learnmore.tsx
+++ components/learnmore.tsx
@@ -1,6 +1,14 @@
 import Services from "./services";
+import Aos from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 export default function Learnmore() {
+  useEffect(() => {
+    Aos.init({
+      duration: 800,
+    });
+  }, []);
   return (
     <main
       id="learnmore"
@@ -11,6 +19,7 @@ export default function Learnmore() {
           <h2
             className="text-3xl md:text-7xl flex items-center justify-center gap-2 font-extrabold
              md:text-center md:leading-tight tracking-tight"
+            data-aos="fade-up"
           >
             وداعاً للقلق، أهلاً
             <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
