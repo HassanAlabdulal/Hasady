1b2728c18378239cd9eaaa69b4e6f7bdba822424
HassanAlabdulal <hhhalabdulaal@gmail.com>
2024-04-15 00:22:16 +0300

    feat: add an icon to the starting button

--- components/start.tsx
+++ components/start.tsx
@@ -40,7 +40,23 @@ export default function Start() {
 
             <div>
               <Button className="rounded-full px-24 py-6 text-xl shadow-2xl bg-black font-normal hover:bg-neutral-800">
-                <Link href="/sign-in">ابدأ الآن </Link>
+                <div className="flex gap-2">
+                  <Link href="/sign-in">ابدأ الآن </Link>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M6.75 15.75 3 12m0 0 3.75-3.75M3 12h18"
+                    />
+                  </svg>
+                </div>
               </Button>
             </div>
           </div>
